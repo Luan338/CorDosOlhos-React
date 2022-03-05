de424e7b2e3e09235a923808c889c8d3a11e2d5b
LnX <luan38.eric@gmail.com>
2022-03-05 05:54:17 -0300

    Finalizando header responsiva

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../Assets/logoNomeBranco.png';
-import Menu from '../../Assets/menu.png';
 import styles from './Header.module.css';
 
 const Header = () => {
@@ -9,28 +8,39 @@ const Header = () => {
   const navRef = useRef();
 
   function activeMenu(){
-    navRef.current.classList.toggle('active');
+    navRef.current?.classList.toggle('active');
   }
+
+
+  window.addEventListener("resize", () => {
+   if(window.matchMedia("(min-width: 890px)").matches){
+    navRef.current?.classList.remove('active');
+   } 
+  })
 
   return (
     <section className={styles.container}>
       <Link className={styles.boxLogo} to="/" aria-label="Cor dos Olhos - Home">
         <img src={Logo} alt="Logo" />
       </Link>
-      <ul className={styles.list}>
-        <Link className={styles.text} to="/">Home</Link>
-        <Link className={styles.text} to="/contato">Contato</Link>
-        <Link className={styles.text} to="/sobre">Sobre</Link>
-        <Link className={styles.text} to="/registrar">Registrar</Link>
-        <Link className={styles.text} to="/consultar">Consultar</Link>
-      </ul>
-      <ul className={styles.login}>
-        <Link className={styles.text} to="/logar">Logar</Link>
-        <span></span>
-        <Link className={styles.textEspecial} to="/cadastrar">Cadastrar</Link>
-      </ul>
-      <div className={styles.menuMobile}>
-        <img src={Menu} alt-="Menu" onClick={activeMenu}/>
+      <nav className={styles.nav} ref={navRef}>
+        <ul className={styles.list}>
+          <Link className={styles.text} to="/">Home</Link>
+          <Link className={styles.text} to="/contato">Contato</Link>
+          <Link className={styles.text} to="/sobre">Sobre</Link>
+          <Link className={styles.text} to="/registrar">Registrar</Link>
+          <Link className={styles.text} to="/consultar">Consultar</Link>
+        </ul>
+        <ul className={styles.login}>
+          <Link className={styles.text} to="/logar">Logar</Link>
+          <span></span>
+          <Link className={styles.textEspecial} to="/cadastrar">Cadastrar</Link>
+        </ul>
+      </nav>
+      <div className={styles.menuMobile} onClick={activeMenu}>
+        <span className={styles.line1}></span>
+        <span className={styles.line2}></span>
+        <span className={styles.line3}></span>
       </div>
     </section>
   )
