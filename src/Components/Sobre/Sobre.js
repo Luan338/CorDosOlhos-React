a5ff60b14e31054bf0e6cc89aecf9c06a83b53c8
LnX <luan38.eric@gmail.com>
2022-03-15 17:48:41 -0300

    Finalizando página sobre

--- src/Components/Sobre/Sobre.js
+++ src/Components/Sobre/Sobre.js
@@ -6,15 +6,44 @@ import Gabi from '../../Assets/gabi.jpeg';
 import homem from '../../Assets/homem.jpg';
 import balao from '../../Assets/balao-fala.png';
 import styles from './Sobre.module.css';
+import { useState } from 'react';
 
 
 const Sobre = () => {
 
-  const cardRef = useRef()
+  const [currentCheckedInfo, setCurrentCheckedInfo] = useState('');
 
-  function activeCard(){
-    cardRef.current?.classList.toggle('onCard');
-    console.log(cardRef)
+  const [info, setInfo] = useState([
+    {
+      img: Luan,
+      balaoImg: balao,
+      nome: "Luan",
+      paragrafo: "Sou carioca e tenho 22 anos de idade, atualmente estou em formação na área Full Stack na Recode Pro, possuo conhecimento com Front-end, e sempre dando o melhor em tudo o que faço."   
+    },
+    {
+      img: Lari,
+      balaoImg: balao,
+      nome: "Lari",
+      paragrafo: "Em formação para ser uma futura programadora full stack, apaixonada por tecnologia e tudo que ela proporciona, fã de arte, design, com um desejo de ajudar a solucionar problemas da sociedade. movida pela curiosidade de descobrir cada vez mais sobre esta especialidade, para garantir mais resultados dos projetos que participa e sempre a procura de novos desafios para aflorar a criatividade."   
+    },
+    {
+      img: Iara,
+      balaoImg: balao,
+      nome: "Iara",
+      paragrafo: "Paulista, casada e mãe de duas maravilhosas crianças, ela é psicóloga e neuropsicóloga, trabalha no processo de autoconhecimento, desenvolvimento e empoderamento feminino. Atua com a perspectiva de melhorar a qualidade de vida, autopercepção, sociabilidade e ressignificação de correntes psíquicas que impactam a saúde e bem estar dos afro brasileiros."   
+    },
+    {
+      img: Gabi,
+      balaoImg: balao,
+      nome: "Gabi",
+      paragrafo: "Vinda de humanas pra programar e trazer esse olhar e vivencias para aproximar cada vez mais o software das pessoas, ama entender como funciona e se atinge o bem estar e cuidado através da tecnologia. Formada em turismo e iniciante teatro."   
+    },
+]);
+
+  function handleClick(item){
+    setCurrentCheckedInfo(
+    item.nome,
+    )
   }
 
   return (
@@ -34,46 +63,23 @@ const Sobre = () => {
         </div>
       </div>
       <section className={styles.aboutUs}>
-        <div className={styles.box}>
-          <div>
-            <img src={Luan} alt="Luan" className={styles.imgLuan}/>
-            <img src={balao} alt="balão de fala" className={styles.balao} onClick={activeCard}/>
-          </div>
-          <div className={styles.card} ref={cardRef}>
-            <h3>Luan</h3>
-            <p>Sou carioca e tenho 22 anos de idade, atualmente estou em formação na área Full Stack na Recode Pro, possuo conhecimento com Front-end, e sempre dando o melhor em tudo o que faço.</p>
-          </div>
+        {info.map((item, key) => {
+          return(
+          <div className={styles.box} key={key}>
+            <div>
+              <img src={item.img} alt="Desenvolvedores" className={styles.imgDevs}/>
+              <img src={item.balaoImg} alt="Balão de fala" className={styles.balao} onClick={() => handleClick(item)}/>
+            </div>
+            {currentCheckedInfo === item.nome &&
+            <div className={styles.card}>
+              <h3>{item.nome}</h3>
+              <p>{item.paragrafo}</p>
+            </div>
+            }
         </div>
-        <div className={styles.box}>
-          <div>
-            <img src={Lari} alt="Lari" className={styles.imgLari}/>
-            <img src={balao} alt="balão de fala" className={styles.balao}/>
-          </div>
-          <div className={styles.card}>
-            <h3>Larissa</h3>
-            <p>Em formação para ser uma futura programadora full stack, apaixonada por tecnologia e tudo que ela proporciona, fã de arte, design, com um desejo de ajudar a solucionar problemas da sociedade. movida pela curiosidade de descobrir cada vez mais sobre esta especialidade, para garantir mais resultados dos projetos que participa e sempre a procura de novos desafios para aflorar a criatividade.</p>
-          </div>
-        </div>
-        <div className={styles.box}>
-          <div>
-            <img src={Iara} alt="Iara" className={styles.imgIara}/>
-            <img src={balao} alt="balão de fala" className={styles.balao}/>
-          </div>
-          <div className={styles.card}>
-            <h3>Iara</h3>
-            <p>Paulista, casada e mãe de duas maravilhosas crianças, ela é psicóloga e neuropsicóloga, trabalha no processo de autoconhecimento, desenvolvimento e empoderamento feminino. Atua com a perspectiva de melhorar a qualidade de vida, autopercepção, sociabilidade e ressignificação de correntes psíquicas que impactam a saúde e bem estar dos afro brasileiros.</p>
-          </div>
-        </div>
-        <div className={styles.box}>
-          <div>
-            <img src={Gabi} alt="Gabi" className={styles.imgGabi}/>
-            <img src={balao} alt="balão de fala" className={styles.balao}/>
-          </div>
-          <div className={styles.card}>
-            <h3>Gabriela</h3>
-            <p>Vinda de humanas pra programar e trazer esse olhar e vivencias para aproximar cada vez mais o software das pessoas, ama entender como funciona e se atinge o bem estar e cuidado através da tecnologia. Formada em turismo e iniciante teatro.</p>
-          </div>
-        </div>
+          )
+        })}
+        
       </section>
     </section>
   )
