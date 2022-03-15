c334c406727fedf6973d8a147a07280371024631
LnX <luan38.eric@gmail.com>
2022-03-15 18:06:31 -0300

    Finalizando página

--- src/Components/Sobre/Sobre.js
+++ src/Components/Sobre/Sobre.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Luan from '../../Assets/luan.jpg';
 import Lari from '../../Assets/lari.jpeg';
 import Iara from '../../Assets/iara.jpeg';
