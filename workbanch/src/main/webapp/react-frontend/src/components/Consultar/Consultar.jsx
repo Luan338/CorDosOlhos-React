e39d72186380769fe5056951d3df20f29d973eb1
LarissaDezuani <dezuanilariss@gmail.com>
2022-03-29 19:38:27 -0300

    nov atualizaacao

--- workbanch/src/main/webapp/react-frontend/src/components/Consultar/Consultar.jsx
+++ workbanch/src/main/webapp/react-frontend/src/components/Consultar/Consultar.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Registro from '../../services/Registro';
-
-class ListRegistro extends Component {
+import styles from './Consultar.module.css';
+import Button from '../Form/Button';
+import Input from '../Form/Input';
+class ConsuRegistro extends Component {
     constructor(props) {
         super(props)
 
@@ -37,12 +39,50 @@ class ListRegistro extends Component {
 
     render() {
         return (
-            <div className='container'>
-                 <h2 className="text-center">Registro List</h2>
-                 <div className = "row">
-                    <button className="btn btn-primary" onClick={this.addRegistro}> Add Registro</button>
-                 </div>
-                 <br></br>
+        <div>  
+            <section className={styles.container}>
+              <div className={styles.boxMap}>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14706.22690269513!2d-43.24328165!3d-22.85588385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1646911883667!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy"></iframe>
+              </div>
+              <div className={styles.animeLeft}>
+                  <div className={styles.boxTitle}>
+                    <h1 className={styles.title}>Verifique a região que deseja saber mais informações</h1>
+                  </div>
+                  <form className={styles.form}>
+                      <Input 
+                      children=""
+                      type="text" class="form-control" name="search" size="50"  placeholder=""   onChange={this.searchBox}
+        
+                      />
+                      {/* <div className={styles.containerInput}>
+                        <Input 
+                        children="Número"
+                        type= "number"
+                        name= "number"
+                        />
+                        <Input 
+                        children="CEP"
+                        type= "text"
+                        name= "cep"
+                        />
+                      </div> */}
+                      {/* <Input 
+                      children="Bairro"
+                      type= "text"
+                      name= "bairro"
+                      />
+                      <Input 
+                      children="Cidade"
+                      type= "text"
+                      name= "cidade"
+                      /> */}
+                      <div className={styles.boxBtn}>
+                        <Button style={{marginLeft:'10px'}} >Cancelar</Button>
+                        <Button >Consultar</Button>
+        
+                      </div>
+            
+                 
 
                  <div className = "table-responsive">
                         <table className = "table table-striped table-bordered">
@@ -50,16 +90,15 @@ class ListRegistro extends Component {
                             <thead>
                                 <tr>
                                     
-                                    <th> endereco</th>
+                                    <th> endereço</th>
                                     <th> numero</th>
                                     <th> cep</th>
                                     <th> bairro</th>
                                     <th> cidade</th>
-                                    <th> pontoreferencia</th>
+                                    <th> ponto de referencia</th>
                                     <th> descreva</th>
-                                    <th> etnia</th>
-                                    <th> genero</th>
-                                    <th>Data</th>
+                                    <th> Data</th>
+                                    
                                    
                                 </tr>
                             </thead>
@@ -76,25 +115,32 @@ class ListRegistro extends Component {
                                              <td> {registro.cidade}</td>
                                              <td> {registro.pontoreferencia}</td>
                                              <td> {registro.descreva} </td>   
-                                             <td> {registro.etnia}</td>
-                                             <td> {registro.genero}</td>
                                              <td> {registro.data}</td>
+                                             
                                              <td>
-                                                 <button onClick={ () => this.editRegistro(registro.id)} className="btn btn-info">Update </button>
-                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.deleteRegistro(registro.id)} className="btn btn-danger">Delete </button>
+                                                 {/* <button onClick={ () => this.editRegistro(registro.id)} className="btn btn-info">Update </button> */}
+                                                 {/* <button style={{marginLeft: "10px"}} onClick={ () => this.deleteRegistro(registro.id)} className="btn btn-danger">Delete </button> */}
                                                  {/* <button style={{marginLeft: "10px"}} onClick={ () => this.viewRegistro(registro.id)} className="btn btn-info">View </button> */}
                                              </td>
                                         </tr>
                                     )
                                 }
                             </tbody>
+                        
                         </table>
 
                  </div>
 
+            
+            
+            </form>
             </div>
+        </section>
+    
+       
+    </div>
         )
     }
 }
 
-export default ListRegistro
+export default ConsuRegistro
